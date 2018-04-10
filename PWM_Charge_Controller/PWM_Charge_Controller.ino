d22bac088ae026e2a35e45d01730fc70117112cb
Gareth Davies <garethdavies.london@gmail.com>
2018-04-10 17:49:26 +0100

    Analogu write and inverter setup
    Adds the analogue writes on via Timer 2. Setting one pin to inverting mode of the other.

--- PWM_Charge_Controller/PWM_Charge_Controller.ino
+++ PWM_Charge_Controller/PWM_Charge_Controller.ino
@@ -1,18 +1,31 @@
 
-#define DELAY 10
+  /*
+   * Based on tutorial from Julian Ilett 
+   * https://www.youtube.com/watch?v=D826h-YQun4
+   */
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(1,OUTPUT);
-  pinMode(2,OUTPUT);
+  Serial.begin(9600);     //Enable serial monitor line
+
+////////////////////////////////
+//
+//    Set up the PWM inverting pins
+//
+///////////////////////////////
+
+  TCCR2A = TCCR2A | 0x30; //This sets bits 4&5 to 1 which puts the time 2 
+                          //Into inverting mode.
+                          //Timer 2 controls pins 11 and 3. 11 Being on A, and 3 on B
+  pinMode(11,OUTPUT);
+  pinMode(3,OUTPUT);
+///////////////////////////////////////
+  
+  analogWrite(11,127);
+  analogWrite(3,127);
+
 }
 
 void loop() {
 
-  digitalWrite(2,LOW);
-  digitalWrite(1,HIGH); 
-  delay(DELAY);
-  digitalWrite(1,LOW);
-  digitalWrite(2,HIGH);
-  delay(DELAY);
+
 }
