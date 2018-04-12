89cffcb1fce8a1a25238398a348252e45f7de6d5
Gareth Davies <garethdavies.london@gmail.com>
2018-04-12 12:46:11 +0100

    Start of adding PWM Class
    Adds the PWM Class

--- PWM_Charge_Controller/PWM_Charge_Controller.ino
+++ PWM_Charge_Controller/PWM_Charge_Controller.ino
@@ -75,10 +75,54 @@ class ChargePumpPWM {
 //////////////////////////////////////////////////////////////////////////////////////////////
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class for Charging PWM waveform control. This implements the various waveforms based on 
+//  Requirements for PWM function of the charge waveform.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+class PWM {
+  private:
+        int state;
+  public:
+        void chargeFull (void);
+        void chargeOff (void);
+        void chargeTrickle (int targetVoltage, int currentVoltage);
+        bool isTrickle(void);
+        bool isOff(void);
+        bool isFull(void);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//END Class PWM
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
 ///GLOBALS
  ChargePumpPWM Mosfet_Highside (CHARGEPUMP_PWM_A,CHARGEPUMP_PWM_B);
 
 
+void WakeUp()
+//This starts up the controller ready to work.
+{
+    //Startup the Charge Pump PWM signal
+    Mosfet_Highside.On();
+}
+
+void Sleep()
+//This puts the Arduino into a low power mode, shuts of the charging circuit.
+{
+    Mosfet_Highside.Off();
+}
+
+
+
+
+
+///////////////////////////////////////////////////
+////////////////MAIN ARDUINO CODE SKETCH ///////////
+///////////////////////////////////////////////////
 
 void setup() {
 
@@ -101,10 +145,6 @@ void loop() {
 
 }
 
-void WakeUp()
-{
-    //Startup the Charge Pump PWM signal
-    Mosfet_Highside.On();
-}
+
 
 
